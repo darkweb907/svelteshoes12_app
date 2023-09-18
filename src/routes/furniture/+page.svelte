bd2c72f645f79df1bcac8ec5091f1022b5f25b1b
darkweb907 <barryopeyemi@gmail.com>
2023-09-18 17:30:19 +0100

    new

--- src/routes/furniture/+page.svelte
+++ src/routes/furniture/+page.svelte
@@ -23,7 +23,9 @@
 	<nav class="container mx-auto bg-black">
 		<Navbar links={navLinks} />
 	</nav>
-	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto mt-5">
+	<div
+		class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 container mx-auto mt-5 max-md:px-4"
+	>
 		{#each furnitureData as item (item.id)}
 			<div
 				class="relative bg-white rounded-lg shadow-lg overflow-hidden"
