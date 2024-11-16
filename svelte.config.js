e4bb15378cb34f059517e0cec28de3fa27ec075c
darkweb907 <barryopeyemi@gmail.com>
2024-11-16 21:25:12 +0100

    added the history aspect

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
 
-export default {
+import adapter from '@sveltejs/adapter-netlify';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: "index.html",
-			
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
 	}
 };
+
+export default config;
